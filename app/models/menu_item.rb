dd371a6a9a50a5d4ae8656727bbfd95e337996e1
Guilherme Simoes <guilherme.rdems@gmail.com>
2012-10-15 23:47:04 +0100

    Made MenuItem's default scope include restaurant and meal.

--- app/models/menu_item.rb
+++ app/models/menu_item.rb
@@ -3,5 +3,7 @@ class MenuItem < ActiveRecord::Base
   belongs_to :meal
   has_many :reservations
 
+  default_scope includes(:restaurant, :meal)
+
   attr_accessible :restaurant_id, :meal_id, :date, :cur_reservations, :max_reservations
 end
