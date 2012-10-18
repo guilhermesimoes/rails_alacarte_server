e766f0984cb6aedfa003c7910e8da6d72a990f3f
Guilherme Simoes <guilherme.rdems@gmail.com>
2012-10-18 17:41:24 +0100

    Added restaurant seeds.

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,11 +7,26 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
+Restaurant.create([ {name: 'Restaurante Luzio Vaz', coordinates: '40.2202,-8.417983'},
+                    {name: 'Restaurante Central', coordinates: '40.208572,-8.421364'},
+                    {name: 'Restaurante Monumentais', coordinates: '40.208564,-8.421353'},
+                    {name: 'Sala B', coordinates: '40.208564,-8.421353'},
+                    {name: 'Restaurante Amarelas', coordinates: '40.208514,-8.420961'},
+                    {name: 'Vegetariana', coordinates: '40.356831,-8.702553'},
+                    {name: 'Sandwich-Bar', coordinates: '40.208467,-8.421025'},
+                    {name: 'Centro Cultural Casa da Pedra', coordinates: '40.18595,-8.417253'},
+                    {name: 'Restaurante das Químicas', coordinates: '40.207417,-8.424156'},
+                    {name: 'Restaurante Vermelhas', coordinates: '40.213753,-8.407453'},
+                    {name: 'Restaurante da Sereia', coordinates: '40.209764,-8.415792'},
+                    {name: 'Restaurante do Estádio Universitário', coordinates: '40.207819,-8.435111'},
+                    {name: 'Restaurante S. Jerónimo', coordinates: '40.209036,-8.422592'},
+                    {name: 'Grill D. Dinis', coordinates: '40.209075,-8.421978'},
+                    {name: 'Cantina do Pólo II', coordinates: '40.1863,-8.414211', address: 'R. Miguel Bombarda, 105, Coimbra, 3030, Portugal' }
+                  ])
+
 User.create({email: 'bob@mail.com', password: 'bob1234', password_confirmation: 'bob1234', name: 'bob', telephone: '12345' })
 
-Restaurant.create({name: 'Cantina do Pólo II', address: 'R. Miguel Bombarda, 105, Coimbra, 3030, Portugal', coordinates: "40°11\'10.68\"N 8°24\'51.16\"W" })
-
-r = Restaurant.create({name: 'Bar de Informática', address: 'R. Miguel Bombarda, 105, Coimbra, 3030, Portugal', coordinates: "40°11\'12.15\"N 8°24\'58.08\"W" })
+r = Restaurant.create({name: 'Bar de Informática', address: 'R. Miguel Bombarda, 105, Coimbra, 3030, Portugal', coordinates: '40.186708,-8.416133' })
 
 m = Meal.create({name: 'Sandes de salsicha', price: 4.20})
 
