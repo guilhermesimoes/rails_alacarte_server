dc8eadcdc92f62be0334e6b75b361ee84aa24624
Guilherme Simoes <guilherme.rdems@gmail.com>
2012-10-22 00:20:32 +0100

    Ahem.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,16 +5,8 @@ class PagesController < ApplicationController
 
   def sms_test
     # Having fun sending messages \o/
-    # guilherme   = User.new({email: 'whatever1@mail.com', telephone: '914355942', name: 'Guilherme'})
-    # penetra     = User.new({email: 'whatever2@mail.com', telephone: '914423862', name: 'Penetra'})
-    # bernardino  = User.new({email: 'whatever3@mail.com', telephone: '916208182', name: 'Bernardino'})
-    # francisco   = User.new({email: 'whatever4@mail.com', telephone: '910772788', name: 'Francisco'})
-    # gaia        = User.new({email: 'whatever5@mail.com', telephone: '916390012', name: 'Gaia'})
-    # UserMailer.reservation_sms(guilherme).deliver
-    # UserMailer.reservation_sms(penetra).deliver
-    # UserMailer.reservation_sms(bernardino).deliver
-    # UserMailer.reservation_sms(francisco).deliver
-    # UserMailer.reservation_sms(gaia).deliver
+    # user = User.new({email: 'whatever@mail.com', telephone: 'cellphone_number', name: 'person_name'})
+    # UserMailer.reservation_sms(user).deliver
     redirect_to root_path, notice: 'Reservation was successful.'
   end
 end
