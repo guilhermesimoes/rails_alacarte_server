ea04cbc23cc4df05bbb0723745aff2d87ede2483
Guilherme Simoes <guilherme.rdems@gmail.com>
2012-10-23 16:38:27 +0100

    Made reservations controller respond to json requests.

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -5,14 +5,30 @@ class ReservationsController < ApplicationController
   # GET /reservations
   # GET /reservations.json
   def index
-    @reservations = current_user.reservations
+    respond_to do |format|
+      format.html {
+        user = current_user
+      }
+      format.json {
+        user = User.find_by_authentication_token(params[:auth_token])
+      }
+    end
+    @reservations = user.reservations
     respond_with(@reservations)
   end
 
   # GET /reservations/1
   # GET /reservations/1.json
   def show
-    @reservation = current_user.reservations.find(params[:id])
+    respond_to do |format|
+      format.html {
+        user = current_user
+      }
+      format.json {
+        user = User.find_by_authentication_token(params[:auth_token])
+      }
+    end
+    @reservation = user.reservations.find(params[:id])
     respond_with(@reservation)
   end
 
@@ -30,47 +46,85 @@ class ReservationsController < ApplicationController
   # POST /reservations
   # POST /reservations.json
   def create
-    @reservation = current_user.reservations.new(params[:reservation])
-
     respond_to do |format|
-      if @reservation.save
-        # Send an SMS informing the user his reservation was successful
-        # UserMailer.reservation_sms(current_user).deliver if current_user.telephone?
+      format.html {
+        user = current_user
 
-        format.html { redirect_to @reservation, notice: 'Reservation was successfully created.' }
-        format.json { render json: @reservation, status: :created, location: @reservation }
-      else
-        format.html { render action: "new" }
-        format.json { render json: @reservation.errors, status: :unprocessable_entity }
-      end
+        @reservation = user.reservations.new(params[:reservation])
+
+        if @reservation.save
+          redirect_to @reservation, notice: 'Reservation was successfully created.'
+        else
+          render action: "new"
+        end
+      }
+      format.json {
+        user = User.find_by_authentication_token(params[:auth_token])
+
+        @reservation = user.reservations.new(params[:reservation])
+
+        if @reservation.save
+
+          # Send an SMS informing the user his reservation was successful
+          # UserMailer.reservation_sms(user).deliver if user.telephone?
+
+          render json: @reservation, status: :created, location: @reservation
+        else
+          render json: @reservation.errors, status: :unprocessable_entity
+        end
+      }
     end
   end
 
   # PUT /reservations/1
   # PUT /reservations/1.json
   def update
-    @reservation = current_user.reservations.find(params[:id])
-
     respond_to do |format|
-      if @reservation.update_attributes(params[:reservation])
-        format.html { redirect_to @reservation, notice: 'Reservation was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: "edit" }
-        format.json { render json: @reservation.errors, status: :unprocessable_entity }
-      end
+      format.html {
+        user = current_user
+
+        @reservation = user.reservations.find(params[:id])
+
+        if @reservation.update_attributes(params[:reservation])
+          redirect_to @reservation, notice: 'Reservation was successfully updated.'
+        else
+          render action: "edit"
+        end
+      }
+      format.json {
+        user = User.find_by_authentication_token(params[:auth_token])
+
+        @reservation = user.reservations.find(params[:id])
+
+        if @reservation.update_attributes(params[:reservation])
+          head :no_content
+        else
+          render json: @reservation.errors, status: :unprocessable_entity
+        end
+      }
     end
   end
 
   # DELETE /reservations/1
   # DELETE /reservations/1.json
   def destroy
-    @reservation = current_user.reservations.find(params[:id])
-    @reservation.destroy
-
     respond_to do |format|
-      format.html { redirect_to reservations_url }
-      format.json { head :no_content }
+      format.html {
+        user = current_user
+
+        @reservation = user.reservations.find(params[:id])
+
+        @reservation.destroy
+        redirect_to reservations_url
+      }
+      format.json {
+        user = User.find_by_authentication_token(params[:auth_token])
+
+        @reservation = user.reservations.find(params[:id])
+
+        @reservation.destroy
+        head :no_content
+      }
     end
   end
 end
