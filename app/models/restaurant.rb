a62b65ce180d081e5b914c8d37275f1afecb84aa
Guilherme Simoes <guilherme.rdems@gmail.com>
2012-10-24 00:59:24 +0100

    Fixed fetching restaurant with all of its meals of the week.

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -3,8 +3,4 @@ class Restaurant < ActiveRecord::Base
   has_many :meals, through: :menu_items
 
   attr_accessible :name, :address, :coordinates, :image
-
-  def self.with_next_seven_meals
-    includes({menu_items: :meal}) #.where("menu_items.date >= ? AND menu_items.date <= ?", Date.today, Date.today+7.days) ... why doesn't this work ...
-  end
 end
--- app/views/restaurants/show.rabl
+++ app/views/restaurants/show.rabl
@@ -1,7 +1,7 @@
 object @restaurant
 attributes :id, :name, :image, :description, :address, :coordinates
 
-child :menu_items => :meals do
+child @menu_items => :meals do
   attributes :date, :max_reservations, :cur_reservations
   glue :meal do
     attributes :name, :price, :image
